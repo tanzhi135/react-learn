8262bcbbecdc6f1ce71f9d80ccc701844f35a295
tanzhi135 <43718175+tanzhi135@users.noreply.github.com>
2026-06-04 14:12:52 +0800

    init
    init files

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite'
+import react, { reactCompilerPreset } from '@vitejs/plugin-react'
+import babel from '@rolldown/plugin-babel'
+import vitePluginCesium from 'vite-plugin-cesium'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    babel({ presets: [reactCompilerPreset()] }),
+    vitePluginCesium()
+  ],
+})
